ed59e261697053f39970d710fa51feaf12472921
HoussameH <houmyh@gmail.com>
2019-11-08 16:51:20 +0100

    Update config.py

--- code/config.py
+++ code/config.py
@@ -52,9 +52,9 @@ def general_settings():
     fs = os.path.sep
     current_folder = os.path.dirname(os.path.abspath(__file__))
     # For personal Computer:
-    root = str(Path(current_folder).parent.parent.parent) + fs + "Database_KS" + fs
+    # root = str(Path(current_folder).parent.parent.parent) + fs + "Database_KS" + fs
     # For Server Computer:
-    # root = str(Path(current_folder).parent.parent.parent) + "Database_KS" + fs
+    root = str(Path(current_folder).parent.parent.parent) + "Database_KS" + fs
 
     return paths, param
 
